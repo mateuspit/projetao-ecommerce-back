c45f701b920e2836f188785c830eb5b02628da6c
Mateus Barcelos <mateuspit@hotmail.com>
2023-04-26 16:31:15 -0300

    set render deploy

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/index.routes.js"
-import apiPort from "./constants/apiPort.js"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(apiPort, () => console.log(`API runing at port ${apiPort}`))
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`API runing at port ${port}`))
