4f874db541e144a6a38561eeeba1ad5ab5156025
joao <joaosb004@gmail.com>
2023-04-26 18:59:04 -0300

    Eslint and prettier final setup

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from "express"
-import cors from "cors"
-import router from "./routes/index.routes.js"
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.routes.js";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(router)
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`API runing at port ${port}`))
+app.listen(port, () => console.log(`API runing at port ${port}`));
