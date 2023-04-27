4bed0d0b54619f8de32c1fccb73a47e92e61359f
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-27 17:48:12 -0300

    feat checkout schema

---
+++ src/schemas/CheckoutSchema.js
@@ -0,0 +1,10 @@
+import joi from "joi";
+
+export const checkoutSchema = joi.object({
+	amount: joi.number().min(1).required(),
+	cpf: joi.string().required(),
+	cardName: joi.string().required(),
+	cardNumber: joi.string().creditCard().required(),
+	expirationDate: joi.date().format("MM/YYYY").required(),
+	cardCVC: joi.string().min(3).required(),
+});
