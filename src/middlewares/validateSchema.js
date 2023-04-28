01f5f89a5e118c90089ea79d6271ddde8f919d32
Mateus Barcelos <mateuspit@hotmail.com>
2023-04-28 03:15:56 -0300

    fixing responde error

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,12 +1,10 @@
 export default function validateSchema(schema) {
-    return (req, res, next) => {
-        const { error } = schema.validate(
-            req.body, { abortEarly: false }
-        )
-        if (error) {
-            const errorMessages = error.details.map(err => err.message);
-            return res.status(422).json({ errors: errorMessages });
-        }
-        next()
-    }
+	return (req, res, next) => {
+		const { error } = schema.validate(req.body, { abortEarly: false });
+		if (error) {
+			const errorMessages = error.details.map((err) => err.message);
+			return res.status(422).json({ errorMessages });
+		}
+		next();
+	};
 }
