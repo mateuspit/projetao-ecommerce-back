82c6c5ed2d9af33167f2bac112f382d2010b1d9e
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-04-28 20:20:58 -0300

    feat credic card schema

--- src/schemas/CheckoutSchema.js
+++ src/schemas/CheckoutSchema.js
@@ -1,10 +1,35 @@
 import joi from "joi";
 
 export const checkoutSchema = joi.object({
-	amount: joi.number().min(1).required(),
-	cpf: joi.string().required(),
-	cardName: joi.string().required(),
-	cardNumber: joi.string().creditCard().required(),
-	expirationDate: joi.date().required(),
-	cardCVC: joi.string().min(3).required(),
+	cardName: joi.string().trim().min(3).max(100).required().messages({
+		"string.min": "O nome do cartão deve ter pelo menos 3 caracteres.",
+		"string.max": "O nome do cartão deve ter no máximo 100 caracteres.",
+		"any.required": "O nome do cartão é obrigatório.",
+	}),
+	cardNumber: joi.string().trim().length(16).creditCard().required().messages({
+		"string.length": "O número do cartão deve ter exatamente 16 dígitos.",
+		"string.creditCard": "O número do cartão de crédito é inválido.",
+		"any.required": "O número do cartão é obrigatório.",
+	}),
+	expirationDate: joi
+		.string()
+		.trim()
+		.regex(/^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/)
+		.required()
+		.messages({
+			"string.pattern.base":
+				"A data de validade deve estar no formato MM/AA ou MM/AAAA.",
+			"any.required": "A data de validade é obrigatória.",
+		}),
+	cardCVC: joi
+		.string()
+		.trim()
+		.length(3)
+		.regex(/^[0-9]{3}$/)
+		.required()
+		.messages({
+			"string.length": "O CVC deve ter exatamente 3 dígitos.",
+			"string.pattern.base": "O CVC deve conter apenas números.",
+			"any.required": "O CVC é obrigatório.",
+		}),
 });
